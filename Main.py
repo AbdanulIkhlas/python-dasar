30abf057a82fa927f249dc6211d47e153d92c8ac
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-08-06 05:48:04 +0700

    initial commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+print("hello world")
+# ini adalah komen
+
+print("=======================================")
+
+"""
+Untuk mencompile file python maka syntax nya
+" python -m py_compile namafile.py "
+
+cara run nya
+" python __pycache__/namafile "
+
+"""
