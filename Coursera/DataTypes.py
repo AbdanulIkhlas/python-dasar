e732ca65ccf7e0039cb1a49b0eb818d9abb87689
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-08-21 09:33:56 +0700

    create folder and add first file exercise from coursera

---
+++ Coursera/DataTypes.py
@@ -0,0 +1,14 @@
+# Check Data Type 
+print(7) # INT
+print("The type is : ")
+print(type(7))
+
+print("")
+print("Hello World") # String
+print("The type is : ")
+print(type("hello"))
+
+print("")
+print(2.5) # Float
+print("The type is : ")
+print(type(2.5))
