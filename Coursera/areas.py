1f5c32513190a2138ebfbd77dc47796260e4abf0
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-08-25 14:50:47 +0700

    add new excercise

---
+++ Coursera/areas.py
@@ -0,0 +1,11 @@
+#! usr/bin/env python3
+import math
+
+def triangle(base,height):
+    return base*height/2
+
+def retangle(base,height):
+    return base*height
+
+def cirle(radius):
+    return math.pi*(radius**2)
