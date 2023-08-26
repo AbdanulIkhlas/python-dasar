23e2ebc5130fa624cb06d713b149aa191c86b2c6
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-08-26 11:26:28 +0700

    tidy up folders

--- ask.py
+++
@@ -1,8 +0,0 @@
-
-def create_python_script(filename):
-  comments = "# Start of a new Python program"
-  with open(filename,"r") as file:
-    filesize = file.getsize(filename)
-  return(filesize)
-
-print(create_python_script("program.py"))
